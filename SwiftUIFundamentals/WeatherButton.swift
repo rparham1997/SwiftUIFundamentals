60bf1d56c59aad5c44373507381406cb8013bc2e
philparham <phil.parham@digital-tectonics.com>
2023-09-11 17:00:23 -0400

    Fundamentals par 1

--- SwiftUIFundamentals/WeatherButton.swift
+++ SwiftUIFundamentals/WeatherButton.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .cornerRadius(10)
     }
 }
 
-struct WeatherButton_Previews: PreviewProvider {
+struct WeatherButton_Preview: PreviewProvider {
     static var previews: some View {
-        WeatherButton()
+        WeatherButton(title: "Test Title",
+                      textColor: .white,
+                      backgroundColor: .blue)
     }
 }
