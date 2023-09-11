4426def16d77c3aa13b5a25dba72cf51ff09eb78
philparham <phil.parham@digital-tectonics.com>
2023-09-10 20:04:11 -0400

    Initial Commit

---
+++ SwiftUIFundamentals/SwiftUIFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentalsApp.swift
+//  SwiftUIFundamentals
+//
+//  Created by Ramar Parham on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
